364d5956a33187e31a373ca67e46d431ae1f6bcf
MD: Anwarul Islam Sheam <anwarul.islamsheam999@gmail.com>
2024-10-12 22:54:01 +0600

    second commit

---
+++ src/assets/component/Review.jsx
@@ -0,0 +1,65 @@
+
+const Review = () => {
+    return (
+        <div>
+            <div className="max-w-screen-xl p-5 mx-auto dark:bg-gray-100 dark:text-gray-800">
+	<div className="grid grid-cols-1 gap-5 lg:grid-cols-4 sm:grid-cols-2">
+		<div className="relative flex items-end justify-start w-full text-left dark:bg-gray-500 bg-center bg-cover h-96" style="background-image: url(&quot;https://source.unsplash.com/random/240x320&quot;);">
+			<div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via- dark:from-gray-50 dark:to-gray-50"></div>
+			<div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+				<a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase dark:text-gray-800 dark:bg-violet-600">Politics</a>
+				<div className="flex flex-col justify-start text-center dark:text-gray-800">
+					<span className="text-3xl font-semibold leading-none tracking-wide">04</span>
+					<span className="leading-none uppercase">Aug</span>
+				</div>
+			</div>
+			<h2 className="z-10 p-5">
+				<a rel="noopener noreferrer" href="#" className="font-medium text-md hover:underline dark:text-gray-800"> Autem sunt tempora mollitia magnam non voluptates</a>
+			</h2>
+		</div>
+		<div className="relative flex items-end justify-start w-full text-left dark:bg-gray-500 bg-center bg-cover h-96" style="background-image: url(&quot;https://source.unsplash.com/random/241x320&quot;);">
+			<div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via- dark:from-gray-50 dark:to-gray-50"></div>
+			<div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+				<a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase dark:text-gray-800 dark:bg-violet-600">Health</a>
+				<div className="flex flex-col justify-start text-center dark:text-gray-800">
+					<span className="text-3xl font-semibold leading-none tracking-wide">01</span>
+					<span className="leading-none uppercase">Aug</span>
+				</div>
+			</div>
+			<h2 className="z-10 p-5">
+				<a rel="noopener noreferrer" href="#" className="font-medium text-md hover:underline dark:text-gray-800">Inventore reiciendis aliquam excepturi</a>
+			</h2>
+		</div>
+		<div className="relative flex items-end justify-start w-full text-left dark:bg-gray-500 bg-center bg-cover h-96" style="background-image: url(&quot;https://source.unsplash.com/random/242x320&quot;);">
+			<div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via- dark:from-gray-50 dark:to-gray-50"></div>
+			<div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+				<a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase dark:text-gray-800 dark:bg-violet-600">Science</a>
+				<div className="flex flex-col justify-start text-center dark:text-gray-800">
+					<span className="text-3xl font-semibold leading-none tracking-wide">28</span>
+					<span className="leading-none uppercase">Jul</span>
+				</div>
+			</div>
+			<h2 className="z-10 p-5">
+				<a rel="noopener noreferrer" href="#" className="font-medium text-md hover:underline dark:text-gray-800">Officiis mollitia dignissimos commodi optio vero animi</a>
+			</h2>
+		</div>
+		<div className="relative flex items-end justify-start w-full text-left dark:bg-gray-500 bg-center bg-cover h-96" style="background-image: url(&quot;https://source.unsplash.com/random/243x320&quot;);">
+			<div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via- dark:from-gray-50 dark:to-gray-50"></div>
+			<div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+				<a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold tracking-wider uppercase dark:text-gray-800 dark:bg-violet-600">Sports</a>
+				<div className="flex flex-col justify-start text-center dark:text-gray-800">
+					<span className="text-3xl font-semibold leading-none tracking-wide">19</span>
+					<span className="leading-none uppercase">Jul</span>
+				</div>
+			</div>
+			<h2 className="z-10 p-5">
+				<a rel="noopener noreferrer" href="#" className="font-medium text-md hover:underline dark:text-gray-800">Doloribus sit illo necessitatibus architecto exercitationem enim</a>
+			</h2>
+		</div>
+	</div>
+</div>
+        </div>
+    );
+};
+
+export default Review;
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,39 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './assets/component/Root';
+import Slider from './assets/component/Slider';
+import Estates from './assets/component/Estates';
+import Ourproject from './assets/component/Ourproject';
+import Review from './assets/component/Review';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "",
+        element: <Slider></Slider>
+      },
+      {
+        path: "/",
+        element: <Estates></Estates>
+      },
+      {
+        path: "/",
+        element: <Ourproject></Ourproject>
+      },
+      {
+        path: "",
+        element: <Review></Review>
+      },
+
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-       <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
